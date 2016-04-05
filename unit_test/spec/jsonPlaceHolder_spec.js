eafe69efb07dbbfa420855af7cb7e5a7bd6c8548
HiroshiOyamada <h5y1m141@gmail.com>
2016-04-06 07:35:05 +0900

    リクエスト送信処理を共通化した処理を呼び出すように定義

--- unit_test/spec/jsonPlaceHolder_spec.js
+++ unit_test/spec/jsonPlaceHolder_spec.js
@@ -11,29 +11,52 @@ describe('JsonPlaceHolder', function() {
     });
   });
   describe('Postsについて', function() {
+    beforeEach(function () {
+      spyOn(this.jsonPlaceHolder.posts, '_request').and.callThrough();
+    });
     describe('indexについて', function() {
       it('定義されてる', function(){
         expect(this.jsonPlaceHolder.posts.index()).toBeDefined();
+      });
+      it('APIを呼び出すための_request()メソッドが呼ばれる', function(){
+        this.jsonPlaceHolder.posts.index();
+        expect(this.jsonPlaceHolder.posts._request).toHaveBeenCalled();
       });
     });
     describe('showについて', function() {
       it('定義されてる', function(){
         expect(this.jsonPlaceHolder.posts.show()).toBeDefined();
       });
+      it('APIを呼び出すための_request()メソッドが呼ばれる', function(){
+        this.jsonPlaceHolder.posts.show();
+        expect(this.jsonPlaceHolder.posts._request).toHaveBeenCalled();
+      });
     });
     describe('createについて', function() {
       it('定義されてる', function(){
         expect(this.jsonPlaceHolder.posts.create()).toBeDefined();
+      });
+      it('APIを呼び出すための_request()メソッドが呼ばれる', function(){
+        this.jsonPlaceHolder.posts.create();
+        expect(this.jsonPlaceHolder.posts._request).toHaveBeenCalled();
       });
     });
     describe('updateについて', function() {
       it('定義されてる', function(){
         expect(this.jsonPlaceHolder.posts.update()).toBeDefined();
       });
+      it('APIを呼び出すための_request()メソッドが呼ばれる', function(){
+        this.jsonPlaceHolder.posts.update();
+        expect(this.jsonPlaceHolder.posts._request).toHaveBeenCalled();
+      });
     });
     describe('destroyについて', function() {
       it('定義されてる', function(){
         expect(this.jsonPlaceHolder.posts.destroy()).toBeDefined();
+      });
+      it('APIを呼び出すための_request()メソッドが呼ばれる', function(){
+        this.jsonPlaceHolder.posts.destroy();
+        expect(this.jsonPlaceHolder.posts._request).toHaveBeenCalled();
       });
     });
   });
--- unit_test/src/jsonPlaceHolder.js
+++ unit_test/src/jsonPlaceHolder.js
@@ -1,24 +1,50 @@
 var JsonPlaceHolder = (function(){
-  function Posts(){
+  function Posts(rootURL){
+    this.rootURL = rootURL;
   }
+  Posts.prototype._request = function(params){
+    var deferred = $.ajax(params);
+    return deferred.promise();    
+  };
   Posts.prototype.index = function(){
-    return true;
+    var params = { 
+      httpMethod: 'GET',
+      params: ''
+    };
+    this.show();
+    return this._request(params);
   };
   Posts.prototype.show = function(){
-    return true;
+    var params = { 
+      httpMethod: 'GET',
+      params: ''
+    };
+    return this._request(params);
   };
   Posts.prototype.create = function(){
-    return true;
+    var params = { 
+      httpMethod: 'GET',
+      params: ''
+    };
+    return this._request(params);
   };  
   Posts.prototype.update = function(){
-    return true;
+    var params = { 
+      httpMethod: 'GET',
+      params: ''
+    };
+    return this._request(params);
   };
   Posts.prototype.destroy = function(){
-    return true;
+    var params = { 
+      httpMethod: 'GET',
+      params: ''
+    };
+    return this._request(params);
   };  
   function JsonPlaceHolder(){
     this.rootURL = 'http://jsonplaceholder.typicode.com';
-    this.posts = new Posts;
+    this.posts = new Posts(this.rootURL);
   };
   
   return JsonPlaceHolder;
